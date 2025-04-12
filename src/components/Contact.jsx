816697ba3cda0075d29438a09e1fb12795df9d41
Venn Edward Nicolas <vdn3903@students.uc-bcf.edu.ph>
2025-04-12 16:39:50 +0800

    Initial Commit

---
+++ src/components/Contact.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Contact() {
+	return (
+		<section className="my-12">
+			<h2 className="text-3xl font-semibold text-blue-600">Contacts</h2>
+			<p className="text-lg mt-4">This is Contacts Section.</p>
+		</section>
+	);
+}
+
+export default Contact;
---
+++ src/components/Education.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Education() {
+	return (
+		<section className="my-12">
+			<h2 className="text-3xl font-semibold text-blue-600">Education</h2>
+			<p className="text-lg mt-4">This is Education Section.</p>
+		</section>
+	);
+}
+
+export default Education;
