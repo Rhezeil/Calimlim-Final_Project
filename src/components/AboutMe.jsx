b6e2123845fbc9076f78e38d129383f07552bc51
RGlei13 <76092@s.ubaguio.edu>
2025-04-15 20:21:25 +0800

    5

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -6,9 +6,9 @@ const AboutMe = () => {
       <h2 className="text-3xl font-bold text-blue-600 hover:text-blue-800 dark:text-blue-400 dark:hover:text-blue-600 transition duration-300">
         About Me
       </h2>
-      <div className="mt-4 text-lg">
+      <div className="text-lg mt-4 leading-relaxed">
         <p>
-          I'm Rhezeil "Glei" Calimlim, an IT student at the University of the Cordilleras, majoring in Network and Security. I'm passionate about tech, fast learner, and I love solving real-world problems through digital solutions.
+          I'm Rhezeil "Glei" Calimlim, an IT student at the University of the Cordilleras.
         </p>
         <div className="my-4 flex justify-center">
           <img src="/images/IMG_6727.jpg" alt="Rhezeil Calimlim" className="rounded-full w-40 h-40 object-cover" />
