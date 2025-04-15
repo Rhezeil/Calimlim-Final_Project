33fd4bf097d70765b93763845732eb707ae9af0b
RGlei13 <76092@s.ubaguio.edu>
2025-04-15 13:11:00 +0800

    something

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import AOS from 'aos';
-import 'aos/dist/aos.css'; // Import AOS styles
+// import 'aos/dist/aos.css'; // Import AOS styles
 
 import AboutMe from './components/AboutMe';
 import Education from './components/Education';
