20c90ae58f28db662de40eb97021cc0b528d1ba3
RGlei13 <76092@s.ubaguio.edu>
2025-04-16 12:43:10 +0800

    a

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -14,7 +14,7 @@ const AboutMe = () => {
         <img
           src="/images/Glei.jpg"  // Correct path to image inside public folder
           alt="Rhezeil Calimlim"
-          className="w-40 h-40 rounded-full object-cover border-2 border-gray-300 dark:border-white shadow-md"
+          className="w-26 h-32 sm:w-30 sm:h-30 rounded-full object-cover border-2 border-gray-300 dark:border-white shadow-md"
         />
 
         </div>
